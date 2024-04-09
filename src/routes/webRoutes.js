8f6b93d26094d7ac2436ee1479b0f4048ed8e409
bmhungqb <21522110@gm.uit.edu.vn>
2024-04-09 20:21:41 +0700

    test upload file api

--- src/routes/webRoutes.js
+++ src/routes/webRoutes.js
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import { homePage, handleLogin } from "../controllers/userController";
+import { hanldExport } from "../controllers/adminController";
 let router = Router();
 
 let initWebRoutes = (app) => {
   // example login
   router.post("/api/login", handleLogin);
   router.get("/", homePage);
+  // admin
+  router.post('/api/exportdata', hanldExport);
   return app.use("/", router);
 };
 
