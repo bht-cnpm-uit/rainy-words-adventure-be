559b1c65e5be14b05eef171012bdc70f1473f75f
lehuudoUIT <22520254@gm.uit.edu.vn>
2024-07-21 22:50:11 +0700

    adjust insert word api

--- src/services/topicService.js
+++ src/services/topicService.js
@@ -25,50 +25,95 @@ const getListTopic = async () => {
     }
   });
 };
+
+const capitalizeFirstLetter = (string) =>
+  string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+
+const normalizeString = (string) => string.trim().toLowerCase();
+
+const mergeObjects = (arr) => {
+  let map = new Map();
+
+  arr.forEach((obj) => {
+    let topicEn = normalizeString(obj.nameEn);
+    let topicVn = normalizeString(obj.nameVi);
+
+    if (map.has(topicEn)) {
+      let existingObj = map.get(topicEn);
+      existingObj.nameVi = capitalizeFirstLetter(topicVn);
+    } else {
+      map.set(topicEn, {
+        nameEn: capitalizeFirstLetter(topicEn),
+        nameVi: capitalizeFirstLetter(topicVn),
+      });
+    }
+  });
+
+  return Array.from(map.values());
+};
+
+const removeDuplicates = (arr1, arr2) => {
+  return arr1.filter(
+    (obj1) =>
+      !arr2.some(
+        (obj2) => obj1.nameEn === obj2.nameEn && obj1.nameVi === obj2.nameVi
+      )
+  );
+};
+
 const createTopic = async (listTopic) => {
   return new Promise(async (resolve, reject) => {
     try {
-      //check before add topic:
-      const uniqueListTopic = Array.from(
-        new Set(listTopic.map((item) => JSON.stringify(item)))
-      ).map((item) => JSON.parse(item));
+      // //check before add topic:
+      // const uniqueListTopic = Array.from(
+      //   new Set(listTopic.map((item) => JSON.stringify(item)))
+      // ).map((item) => JSON.parse(item));
 
-      // Kiểm tra có phần tử bị lặp trong mảng truyền vào
-      if (listTopic.length !== uniqueListTopic.length) {
-        return resolve({
-          errCode: 4,
-          message: "Create list topic unsuccessfully!",
-          error: "There are duplicate elements in the input list topic.",
-        });
-      }
+      // // Kiểm tra có phần tử bị lặp trong mảng truyền vào
+      // if (listTopic.length !== uniqueListTopic.length) {
+      //   return resolve({
+      //     errCode: 4,
+      //     message: "Create list topic unsuccessfully!",
+      //     error: "There are duplicate elements in the input list topic.",
+      //   });
+      // }
 
       let currentListTopic = await db.Topic.findAll({
         raw: true,
         attributes: ["nameEn", "nameVi"],
       });
 
-      const set1 = new Set(listTopic.map((item) => JSON.stringify(item)));
-      const set2 = new Set(
-        currentListTopic.map((item) => JSON.stringify(item))
+      // const set1 = new Set(listTopic.map((item) => JSON.stringify(item)));
+      // const set2 = new Set(
+      //   currentListTopic.map((item) => JSON.stringify(item))
+      // );
+
+      // // Kiểm tra xem có phần tử nào trong set1 tồn tại trong set2
+      // const hasDuplicates = [...set1].some((item) => set2.has(item));
+
+      // if (hasDuplicates) {
+      //   return resolve({
+      //     errCode: 5,
+      //     message: "Create list topic unsuccessfully!",
+      //     error:
+      //       "There are duplicate elements between input list topic and current list topic.",
+      //   });
+      // }
+
+      let normalizeTopics = mergeObjects(listTopic);
+
+      let removeDuplicateTopics = removeDuplicates(
+        normalizeTopics,
+        currentListTopic
       );
 
-      // Kiểm tra xem có phần tử nào trong set1 tồn tại trong set2
-      const hasDuplicates = [...set1].some((item) => set2.has(item));
-
-      if (hasDuplicates) {
-        return resolve({
-          errCode: 5,
-          message: "Create list topic unsuccessfully!",
-          error:
-            "There are duplicate elements between input list topic and current list topic.",
+      if (removeDuplicateTopics.length > 0) {
+        await db.Topic.bulkCreate(removeDuplicateTopics, {}).catch((err) => {
+          console.log(err);
         });
       }
 
-      await db.Topic.bulkCreate(listTopic).catch((err) => {
-        console.log(err);
-      });
-
-      resolve({
+      return resolve({
         errCode: 0,
         message: "Create list topic successfully!",
       });
--- src/services/wordService.js
+++ src/services/wordService.js
@@ -1,4 +1,5 @@
 import db from "../models/index";
+import { createTopic } from "./topicService";
 
 const getListWord = async () => {
   return new Promise(async (resolve, reject) => {
@@ -25,23 +26,58 @@ const getListWord = async () => {
     }
   });
 };
+
+const normalizeString = (string) =>
+  string.trim().toLowerCase().replace(/\s+/g, "");
+
+const compareStrings = (str1, str2) => {
+  return normalizeString(str1) === normalizeString(str2);
+};
+
 const createWord = async (listWord) => {
   return new Promise(async (resolve, reject) => {
     try {
-      console.log(listWord);
+      //! Tạo topic
+      let listTopic = listWord.map((word) => {
+        return {
+          nameEn: word.nameEn,
+          nameVi: word.nameVi,
+        };
+      });
 
-      db.Word.bulkCreate(listWord)
-        .catch((err) => {
-          console.log(err);
-        })
-        .catch((err) => console.log(err));
+      await createTopic(listTopic);
+
+      //! Lấy danh sách list topic sau khi được thêm
+      let currentListTopic = await db.Topic.findAll({
+        raw: true,
+        attributes: ["id", "nameEn", "nameVi"],
+      });
+
+      //! Insert từ vựng
+
+      let listWordNormalize = listWord.map((item) => {
+        let result = currentListTopic.find(
+          (topic) =>
+            compareStrings(topic.nameEn, item.nameEn) &&
+            compareStrings(topic.nameVi, item.nameVi)
+        );
+
+        return {
+          ...item,
+          topicId: result.id,
+        };
+      });
+
+      await db.Word.bulkCreate(listWordNormalize).catch((err) => {
+        console.log(err);
+      });
 
       resolve({
         errCode: 0,
         message: "Create listWord successfully!",
       });
     } catch (error) {
-      reject({
+      resolve({
         errCode: 2,
         message: "Create listWord unsuccessfully!",
         error: error,
